75d7a5eb99ca226aed24d3505a1b5f2312ac77bb
Dar Strommer <dar.strommer@gmail.com>
2024-03-30 11:50:11 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import invite from './invite.png';
 import './App.css';
 
-function App() {
+export default function App() {
+  function Submit(e) {
+    e.preventDefault();
+
+    const formEle = document.querySelector('form');
+    const formDatab = new FormData(formEle);
+    fetch(
+      'https://script.google.com/macros/s/AKfycbx0TLUKdN8vD1Z9SoVh6j5nssKVaVY0UF-i0mfRZB5GvUCJ-cXP9Edl7RsuWwOD6A/exec',
+      {
+        method: 'POST',
+        body: formDatab,
+      }
+    )
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <img alt='' src={invite} />
+      <form className='form-wrapper' onSubmit={(e) => Submit(e)}>
+        <div className='input-wrapper'>
+          <p>?כמה תגיעו</p>
+          <input placeholder={0} name='guests' type='number' />
+        </div>
+        <div className='input-wrapper'>
+          <p>?צריכים הסעה</p>
+          <input placeholder={false} name='ride' type='checkbox' />
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
-
-export default App;
